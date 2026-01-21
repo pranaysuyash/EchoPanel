5b210c660246fa9cb971951fe57a90a3c03ad4cc
Pranay Suyash <pranay.suyash@gmail.com>
2026-01-21 15:30:18 +0530

    Ensure side panel opens in front

--- macapp/MeetingListenerApp/Sources/SidePanelController.swift
+++ macapp/MeetingListenerApp/Sources/SidePanelController.swift
@@ -6,33 +6,38 @@ final class SidePanelController {
     private var hostingController: NSHostingController<SidePanelView>?
 
     func show(appState: AppState, onEndSession: @escaping () -> Void) {
-        if panel == nil {
-            let view = SidePanelView(appState: appState, onEndSession: onEndSession)
-            let host = NSHostingController(rootView: view)
-            hostingController = host
+        DispatchQueue.main.async {
+            if self.panel == nil {
+                let view = SidePanelView(appState: appState, onEndSession: onEndSession)
+                let host = NSHostingController(rootView: view)
+                self.hostingController = host
 
-            let panel = NSPanel(
-                contentRect: NSRect(x: 0, y: 0, width: 960, height: 520),
-                styleMask: [.titled, .closable, .resizable, .utilityWindow],
-                backing: .buffered,
-                defer: false
-            )
-            panel.title = "EchoPanel"
-            panel.isFloatingPanel = true
-            panel.level = .floating
-            panel.contentViewController = host
-            self.panel = panel
-        } else if let hostingController {
-            hostingController.rootView = SidePanelView(appState: appState, onEndSession: onEndSession)
+                let panel = NSPanel(
+                    contentRect: NSRect(x: 0, y: 0, width: 960, height: 520),
+                    styleMask: [.titled, .closable, .resizable, .utilityWindow],
+                    backing: .buffered,
+                    defer: false
+                )
+                panel.title = "EchoPanel"
+                panel.isFloatingPanel = true
+                panel.level = .floating
+                panel.hidesOnDeactivate = false
+                panel.isReleasedWhenClosed = false
+                panel.collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary, .moveToActiveSpace]
+                panel.contentViewController = host
+                self.panel = panel
+            } else if let hostingController = self.hostingController {
+                hostingController.rootView = SidePanelView(appState: appState, onEndSession: onEndSession)
+            }
+
+            NSApp.activate(ignoringOtherApps: true)
+            self.panel?.center()
+            self.panel?.makeKeyAndOrderFront(nil)
+            self.panel?.orderFrontRegardless()
         }
-
-        panel?.center()
-        panel?.makeKeyAndOrderFront(nil)
-        NSApp.activate(ignoringOtherApps: true)
     }
 
     func hide() {
         panel?.orderOut(nil)
     }
 }
-
